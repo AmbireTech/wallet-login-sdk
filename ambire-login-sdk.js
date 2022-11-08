50fbb1142197c7892c7829f974f3811746a40337
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2022-11-08 18:46:18 +0200

    Add AmbireSDK skeleton with basic methods for calling modals and listening to events

--- ambire-login-sdk.js
+++ ambire-login-sdk.js
@@ -1,3 +1,86 @@
-function getLoginIframe() {
-    return `<iframe src="http://localhost:3000/#/email-login-iframe" width="100%" height="100%" frameborder="0"/>`
+window.AmbireSDK = function (opt = {}) {
+    const self = this
+
+    this.dappName = opt.dappName ?? 'Unknown Dapp'
+    this.iframeElement = document.getElementById(opt.iframeElementId ?? "ambire-sdk-iframe")
+    this.connectButton = document.getElementById(opt.connectButtonId ?? "ambire-sdk-connect-btn")
+    this.logoutButton = document.getElementById(opt.logoutButtonId ?? "ambire-sdk-logout-btn")
+    this.addressElement = document.getElementById(opt.addressElementId ?? "ambire-sdk-wallet-address")
+
+    // init
+    const wallet = window.localStorage.getItem('wallet_address')
+    if (wallet) {
+        this.addressElement.innerHTML = `Wallet address: ${wallet}`
+        this.connectButton.style.display = 'none'
+        this.logoutButton.style.display = 'block'
+    } else {
+        this.addressElement.innerHTML = ''
+        this.connectButton.style.display = 'block'
+        this.logoutButton.style.display = 'none'
+    }
+    this.connectButton.addEventListener('click', function() {
+        self.openLogin()
+    })
+    this.logoutButton.addEventListener('click', function() {
+        self.logout()
+    })
+
+    this.openLogin = function() {
+        // TODO
+
+        // temp code
+        self.connectButton.style.display = 'none'
+        self.iframeElement.style.width = '100%'
+        self.iframeElement.style.height = '600px'    
+        self.iframeElement.innerHTML = `<iframe src="http://localhost:3000/#/email-login-iframe" width="100%" height="100%" frameborder="0"/>`
+    }
+
+    this.openSignMessage = function() {
+        // TODO
+    }
+
+    this.openSendTransaction = function() {
+        // TODO
+    }
+
+    this.logout = function() {
+        // TODO
+
+        if (!window.localStorage.getItem('wallet_address')) return
+        
+        window.localStorage.removeItem('wallet_address')
+        self.logoutButton.style.display = 'none'
+        self.addressElement.innerHTML = ''
+        self.connectButton.style.display = 'block'
+    }
+
+    // emit event
+    this.emit = function(eventName, data = {}) {
+        const event = new CustomEvent(eventName, { detail: { ...data }})
+        window.dispatchEvent(event)
+        console.log(`${eventName} was dispatched`)
+    }
+
+    // generic event listener
+    this.on = function(eventName, callback) {
+        // console.log(`${eventName} was received`)
+        window.addEventListener(eventName, function(event) {
+            callback(event)
+        })
+    }
+
+    // ambire-login-success listener
+    this.onLoginSuccess = function(callback) {
+        window.addEventListener('message', (e) => {
+            if (e.origin !== 'http://localhost:3000') return
+
+            // console.log(`ambire login details: ${JSON.stringify(e.data)}`)
+            self.addressElement.innerHTML = `Wallet address: ${e.data.address}`
+            self.iframeElement.remove()
+            self.logoutButton.style.display = 'block'
+            window.localStorage.setItem('wallet_address', e.data.address)
+
+            callback(e.data.address)
+        }, false)
+    }
 }
--- client-script.js
+++ client-script.js
@@ -1,45 +1,10 @@
-const connectButton = document.getElementById("connect")
-const logoutButton = document.getElementById("logout")
-const addressElement = document.getElementById("wallet-address")
-const iframeElement = document.getElementById("ambire-sdk-iframe")
-
-window.addEventListener('message', (e) => {
-    if (e.origin !== 'http://localhost:3000') return
-
-    console.log(`ambire login details: ${JSON.stringify(e.data)}`)
-    addressElement.innerHTML = `Wallet address: ${e.data.address}`
-    iframeElement.remove()
-
-    window.localStorage.setItem('wallet_address', e.data.address)
-    logoutButton.style.display = 'block'
-}, false)
-
-connectButton.addEventListener('click', function(){
-    this.style.display = 'none'
-    // let iframeElement = iframeDiv
-    iframeElement.style.width = '100%'
-    iframeElement.style.height = '600px'
-
-    let iframe = getLoginIframe()
-    iframeElement.innerHTML = iframe
+const sdk = new window.AmbireSDK({
+    dappName: 'dapp1',
+    iframeElementId: 'ambire-sdk-iframe',
+    connectButtonId: 'connect',
+    logoutButtonId: 'logout',
+    addressElementId: 'wallet-address',
 })
-
-logoutButton.addEventListener('click', function(){
-    if (!window.localStorage.getItem('wallet_address')) return
-
-    window.localStorage.removeItem('wallet_address')
-    this.style.display = 'none'
-    addressElement.innerHTML = ''
-    connectButton.style.display = 'block'
+sdk.onLoginSuccess(function(address) {
+    console.log(`Client onLoginSuccess, address is: ${address}`)
 })
-
-const wallet = window.localStorage.getItem('wallet_address')
-if (wallet) {
-    addressElement.innerHTML = `Wallet address: ${wallet}`
-    connectButton.style.display = 'none'
-    logoutButton.style.display = 'block'
-} else {
-    connectButton.style.display = 'block'
-    addressElement.innerHTML = ''
-    logoutButton.style.display = 'none'
-}
