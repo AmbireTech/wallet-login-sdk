b7e9753178e1e95ed4ac1f4266d4a4f7e2c96dd1
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2023-02-14 14:46:55 +0200

    Update author, licence and publishConfig settings in package.json

--- packages/core/src/sdk-core.ts
+++ packages/core/src/sdk-core.ts
@@ -11,7 +11,7 @@ class AmbireLoginSDK {
 
   constructor(opt:  sdkParamsType) {
     this.walletUrl = opt.walletUrl ?? 'Unknown Dapp'
-    this.dappName = opt.dappName ?? 'Unknown Dapp'
+    this.dappName = opt.dappName ?? 'Unknown Dapp 1'
     this.dappIconPath = opt.dappIconPath ?? ''
     this.wrapperElementId = opt.wrapperElementId ?? 'ambire-sdk-wrapper'
     this.wrapperElement = null
